eb19cee20b32c51f9e4b96dc10a065f21b7be455
Konthaina <thainakon87@gmail.com>
2026-01-26 16:34:30 +0700

    change format

--- dist-with-date.cjs
+++ dist-with-date.cjs
@@ -5,7 +5,7 @@ function formatDate(d) {
   const yyyy = d.getFullYear();
   const mm = String(d.getMonth() + 1).padStart(2, "0");
   const dd = String(d.getDate()).padStart(2, "0");
-  return `${yyyy}-${mm}-${dd}`;
+  return `${yyyy}${mm}${dd}`;
 }
 
 const releaseDate = formatDate(new Date());
